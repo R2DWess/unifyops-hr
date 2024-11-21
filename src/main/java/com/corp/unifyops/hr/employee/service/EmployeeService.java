0d712e7645fc2a58967162b280dc8885c265ac06
R2DWess <gusterlima@hotmail.com>
2024-11-21 00:20:36 -0300

    fix: correct naming from 'Employerr' to 'Employee'

--- src/main/java/com/corp/unifyops/hr/employee/service/EmployeeService.java
+++ src/main/java/com/corp/unifyops/hr/employee/service/EmployeeService.java
@@ -1,9 +1,8 @@
 package com.corp.unifyops.hr.employee.service;
 import com.corp.unifyops.hr.dto.EmployeeDTO;
-import com.corp.unifyops.hr.employee.model.EmployeeData;
 import jakarta.transaction.Transactional;
 
-public interface EmployerrService {
+public interface EmployeeService {
 
     EmployeeDTO register(EmployeeDTO employeeDTO);
 
