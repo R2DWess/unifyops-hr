9f470f9cd21ec0a1c89641e6cee8b1059ae3acbe
R2DWess <gusterlima@hotmail.com>
2024-11-21 00:20:01 -0300

    fix: correct naming from 'Employerr' to 'Employee'

--- src/main/java/com/corp/unifyops/hr/employee/controller/EmployeeController.java
+++ src/main/java/com/corp/unifyops/hr/employee/controller/EmployeeController.java
@@ -1,7 +1,7 @@
 package com.corp.unifyops.hr.employee.controller;
 
 import com.corp.unifyops.hr.dto.EmployeeDTO;
-import com.corp.unifyops.hr.employee.service.EmployerrService;
+import com.corp.unifyops.hr.employee.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,25 +10,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    private final EmployerrService employerrService;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public EmployeeController(EmployerrService employerrService) {
-        this.employerrService = employerrService;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @PostMapping
     public EmployeeDTO register(@Valid @RequestBody EmployeeDTO employeeDTO) {
-        return employerrService.register(employeeDTO);
+        return employeeService.register(employeeDTO);
     }
 
     @PatchMapping("/{employeeId}")
     public EmployeeDTO update(@PathVariable String employeeId, @RequestBody EmployeeDTO employeeDTO) {
-        return employerrService.update(employeeId, employeeDTO);
+        return employeeService.update(employeeId, employeeDTO);
     }
 
     @DeleteMapping("/{employeeId}")
     public void delete(@PathVariable String employeeId) {
-        employerrService.delete(employeeId);
+        employeeService.delete(employeeId);
     }
 }
