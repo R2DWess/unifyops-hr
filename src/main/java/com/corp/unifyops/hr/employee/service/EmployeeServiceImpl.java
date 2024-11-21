439fa7fd833cfeea1d69bee4d9c4909b7b0db8b3
R2DWess <gusterlima@hotmail.com>
2024-11-21 00:22:13 -0300

    fix: add class for generating employee functional ID

--- src/main/java/com/corp/unifyops/hr/employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/corp/unifyops/hr/employee/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.corp.unifyops.hr.dto.EmployeeDTO;
 import com.corp.unifyops.hr.employee.model.EmployeeData;
 import com.corp.unifyops.hr.employee.repository.EmployeeRepository;
 import com.corp.unifyops.hr.mapper.EmployeeMapper;
+import com.corp.unifyops.hr.utils.EmployeeIdGenerator; // Importação do gerador de ID
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,18 +12,21 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class EmployerrServiceImpl implements EmployerrService {
+public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final EmployeeIdGenerator employeeIdGenerator;
 
     @Autowired
-    public EmployerrServiceImpl(EmployeeRepository employeeRepository) {
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, EmployeeIdGenerator employeeIdGenerator) {
         this.employeeRepository = employeeRepository;
+        this.employeeIdGenerator = employeeIdGenerator;
     }
 
     @Override
     public EmployeeDTO register(EmployeeDTO employeeDTO) {
         EmployeeData employeeData = EmployeeMapper.INSTANCE.toEntity(employeeDTO);
+        employeeData.setEmployeeId(employeeIdGenerator.uniqueIdGenerator());
         EmployeeData savedEmployee = employeeRepository.save(employeeData);
         return EmployeeMapper.INSTANCE.toDTO(savedEmployee);
     }
@@ -35,8 +39,7 @@ public class EmployerrServiceImpl implements EmployerrService {
 
     @Override
     public EmployeeDTO update(String employeeId, EmployeeDTO employeeDTO) {
-        Optional<EmployeeData>
-                existingEmployee = employeeRepository.findByEmployeeId(employeeId);
+        Optional<EmployeeData> existingEmployee = employeeRepository.findByEmployeeId(employeeId);
         if (existingEmployee.isPresent()) {
             EmployeeData employeeData = existingEmployee.get();
             EmployeeMapper.INSTANCE.updateEntityFromDTO(employeeDTO, employeeData);
