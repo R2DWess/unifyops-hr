1d4aacb6d578cfc741d6ed376e01f934431d8df8
R2DWess <gusterlima@hotmail.com>
2024-11-21 01:22:34 -0300

    refactor: rebuild exposed data in the properties file

--- src/main/java/com/corp/unifyops/hr/UnifyOpsApplication.java
+++ src/main/java/com/corp/unifyops/hr/UnifyOpsApplication.java
@@ -1,13 +1,17 @@
 package com.corp.unifyops.hr;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UnifyOpsApplication {
-
     public static void main(String[] args) {
+        // Carregar o arquivo .env
+        Dotenv dotenv = Dotenv.configure().load();
+        // Definir variáveis de ambiente manualmente
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(UnifyOpsApplication.class, args);
     }
-
 }
